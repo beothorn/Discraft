96e67e7a7e3581a1510ddaf6a330ee5560979115
Beothorn <lucass@gmail.com>
2019-05-23 20:33:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@ sudo apt-get update
 sudo apt-get -y install python3-pip
 pip3 install discord.py==0.16.12
 pip3 install pexpect
+sudo apt install openjdk-11-jre-headless
+wget https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar
+java -jar server.jar
+sed -i "s/false/true/" eula.txt
